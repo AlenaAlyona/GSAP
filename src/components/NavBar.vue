7a1045b75b0ee6eb3a21aaf58ed94ad8cbd81a38
AlenaAlyona <izakson91@gmail.com>
2021-10-22 16:28:53 +0200

    task: add content

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,55 @@
 <template>
-  <div class="about">
-    <h1>Navbar</h1>
+  <div class="menu-open">menu</div>
+  <div class="nav-container">
+    <div class="menu-close">close</div>
+    <div class="socials">
+      <span>github</span>
+      <span>linkedin</span>
+    </div>
+    <nav class="menu">
+      <div class="menu-item">
+        <a class="menu-item-link">Home</a>
+        <img class="menu-item-img" src="@/assets/images/lain.jpeg" />
+        <div class="marquee">
+          <div class="marquee-inner">
+            <span>Home - Home - Home - Home - Home - Home - Home</span>
+          </div>
+        </div>
+      </div>
+      <div class="menu-item">
+        <a class="menu-item-link">Showcase</a>
+        <img class="menu-item-img" src="@/assets/images/lain.jpeg" />
+        <div class="marquee">
+          <div class="marquee-inner">
+            <span
+              >Showcase - Showcase - Showcase - Showcase - Showcase - Showcase -
+              Showcase</span
+            >
+          </div>
+        </div>
+      </div>
+      <div class="menu-item">
+        <a class="menu-item-link">About</a>
+        <img class="menu-item-img" src="@/assets/images/lain.jpeg" />
+        <div class="marquee">
+          <div class="marquee-inner">
+            <span>About - About - About - About - About - About - About</span>
+          </div>
+        </div>
+      </div>
+      <div class="menu-item">
+        <a class="menu-item-link">Contact</a>
+        <img class="menu-item-img" src="@/assets/images/lain.jpeg" />
+        <div class="marquee">
+          <div class="marquee-inner">
+            <span
+              >Contact - Contact - Contact - Contact - Contact - Contact -
+              Contact</span
+            >
+          </div>
+        </div>
+      </div>
+    </nav>
   </div>
 </template>
 <script>
