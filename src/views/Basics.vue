0cf9b49d41fd58a3865dc379953b3704a03a4bdb
AlenaAlyona <izakson91@gmail.com>
2021-10-25 11:27:41 +0200

    task: add content to Basics

---
+++ src/views/Basics.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="basics container-images">
+    <img class="img1" src="../assets/images/cat.svg" alt="chat" />
+    <img class="img2" src="../assets/images/fuji.svg" alt="mount fuji" />
+    <img class="img3" src="../assets/images/ice-cream.svg" alt="ice cream" />
+  </div>
+
+  <h1 class="title">Welcome to the sandbox.</h1>
+  <p class="txt">Let's play with Greensock.</p>
+
+  <!-- Stagger -->
+
+  <!-- <div class="container-btns">
+    <button class="btn">CONFIRM</button>
+    <button class="btn">CONFIRM</button>
+    <button class="btn">CONFIRM</button>
+    <button class="btn">CONFIRM</button>
+    <button class="btn">CONFIRM</button>
+    <button class="btn">CONFIRM</button>
+    </div>  -->
+</template>
+
+<style scoped>
+.basics {
+  background: #222;
+  overflow: hidden;
+}
+</style>
