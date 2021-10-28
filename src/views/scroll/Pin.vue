69ee82311de721ac605283352a669f4782e1f129
AlenaAlyona <izakson91@gmail.com>
2021-10-29 01:32:01 +0200

    task: create Pin view

---
+++ src/views/scroll/Pin.vue
@@ -0,0 +1,282 @@
+<template>
+  <div class="pin-project">
+    <div class="pin-home">
+      <h1>HISTORICAL PERIODS</h1>
+    </div>
+    <div class="pin-tooltip">
+      <img class="pin-tooltip-img" src="../../assets/scroll/pin/spartan.svg" />
+      <p>-3300 | 476</p>
+    </div>
+    <div class="pin-sections">
+      <div class="pin-section pin-s1">
+        <div class="pin-content">
+          <h2>ANTIQUITY.</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+            inventore soluta reiciendis exercitationem culpa earum rem at libero
+            laudantium ut? Minus nisi maxime commodi sit obcaecati corrupti
+            debitis eum delectus incidunt eveniet soluta dicta voluptate
+            repellat placeat tempore inventore similique deleniti accusamus quos
+            necessitatibus, eaque itaque impedit? Repudiandae animi ipsam aut
+            eius obcaecati aspernatur reprehenderit qui vel officia consectetur
+            omnis ducimus asperiores sed, at architecto assumenda nulla esse?
+            Aliquam voluptas numquam delectus, sunt consectetur, maxime
+            perferendis placeat libero est quae illo nobis! Repudiandae aliquam
+            architecto nobis iure inventore eaque. At quia magnam odit dolorum
+            veniam aspernatur molestiae vero aperiam? Possimus, excepturi in
+            reprehenderit quas at dolores rerum natus architecto velit numquam
+            eligendi libero minus illo illum, deleniti deserunt ipsa neque
+            sapiente ex quaerat quibusdam fugit reiciendis esse. Soluta, tempora
+            doloribus impedit ab sequi, libero nesciunt quas culpa nobis
+            inventore neque velit quae reiciendis amet dignissimos optio
+            pariatur? Facilis, quia at. Laboriosam tenetur ipsam autem aliquid
+            ad. Harum omnis architecto nisi impedit sequi cumque minima
+            voluptates vero. Voluptatibus veniam, voluptatum natus minima
+            dolorum doloremque nobis aliquam? Quibusdam rerum non, totam earum
+            fuga praesentium aliquam nihil numquam officia dolore fugit maiores
+            voluptatum deserunt, eveniet quis fugiat sapiente ipsam vitae
+            facilis harum perspiciatis. Asperiores explicabo ratione totam est
+            delectus obcaecati distinctio expedita veritatis necessitatibus.
+            Suscipit quos libero ipsam magni. Ipsum illum nobis, facilis eaque
+            vero error odit modi, incidunt voluptate repudiandae aliquid ad vel
+            at aliquam deleniti officia rem eveniet. A odit, deserunt
+            repellendus blanditiis magni saepe quo reprehenderit, ipsum sit,
+            dolorum nulla? Delectus veritatis magni voluptatibus fugit ab porro
+            iste odio molestiae laborum voluptate nostrum cumque quibusdam
+            quisquam accusantium eius natus non quam nisi numquam, voluptates
+            debitis corrupti facilis. Laborum beatae asperiores ut tempora cum
+            eaque illo, neque exercitationem quos? Laboriosam nemo corrupti quae
+            reprehenderit dolorum sed ipsa. Eos id laboriosam minus!
+          </p>
+        </div>
+      </div>
+      <div class="pin-section pin-s2">
+        <div class="pin-content">
+          <h2>MIDDLE AGE.</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            suscipit, eligendi a aperiam vel ex quod at debitis, sint explicabo
+            nostrum natus, recusandae est. Mollitia amet laborum accusamus sequi
+            expedita quo nesciunt quis sint tempora amet iure mollitia magnam
+            quam aperiam, cum earum. Adipisci libero qui quo id totam. Quidem
+            labore ullam laboriosam beatae blanditiis soluta a minus provident
+            alias consequatur unde sapiente, repudiandae eum, modi, officiis
+            ipsa enim reprehenderit praesentium illum consequuntur assumenda in
+            rerum dignissimos quis. Necessitatibus quam autem dolores enim quos
+            quiaectus sapiente a cum excepturi, expedita odio natus pariatur?
+            Repellat tempore id autem blanditiis odit architecto pariatur
+            provident ipsum nemo. Facilis reprehenderit, soluta, rem officiis,
+            ipsa nobis ex quia quasi harum reiciendis sunt nam quam quos!
+          </p>
+        </div>
+      </div>
+      <div class="pin-section pin-s3">
+        <div class="pin-content">
+          <h2>MODERN TIMES.</h2>
+          <p>
+            aperiam, cum earum. Adipisci libero qui quo id totam. Quidem labore
+            ullam laboriosam beatae blanditiis soluta a minus provident alias
+            consequatur unde sapiente, repudiandae eum, modi, officiis ipsa enim
+            reprehenderit praesentium illum consequuntur assumenda in rerum
+            dignissimos quis. Necessitatibus quam autem dolores enim quos quia
+            natus earum commodi tenetur, suscipit expedita ea deserunt fugiat
+            incidunt, provident exercitationem dolor vitae quisquam! Praesentium
+            ullam delectus sapiente a cum excepturi, expedita odio natus
+            pariatur? Repellat tempore id autem blanditiis odit architecto
+            pariatur provident ipsum nemo. Facilis reprehenderit, soluta, rem
+            officiis, ipsa nobis ex quia quasi harum reiciendis sunt nam quam
+            quos!
+          </p>
+        </div>
+      </div>
+      <div class="pin-section pin-s4">
+        <div class="pin-content">
+          <h2>Contemporary period.</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+            nemo porro eveniet consectetur quo adipisci soluta voluptatum
+            voluptatem illo suscipit nihil cupiditate eos eum iusto odit saepe
+            sint, quia itaque placeat accusamus sit alias. Quos sint aspernatur,
+            quaerat odio suscipit, totam, vitae rerum assumenda provident sequi
+            itaque quas sed tempora. Quibusdam natus vero, dicta autem odio
+            cupiditate voluptatem reprehenderit tenetur vel officia nobis, rerum
+            ipsam, pariatur rem nesciunt? Provident soluta voluptate molestiae
+            recusandae veritatis, libero laudantium assumenda officiis ratione
+            aliquam esse earum reiciendis fuga quae amet accusantium illo sit
+            consequuntur obcaecati! Dolore, ullam eos eum deleniti quod, earum
+            ipsa dolores a nam consequatur cum officia totam iure fugit.
+            Eligendi, at fuga? Voluptatibus repellendus voluptates, unde id
+            itaque perferendis obcaecati nemo dolor fugiat ullam sed accusamus
+            voluptate quibusdam nihil quam omnis at consequuntur voluptatum,
+            placeat corrupti inventore est. Omnis a incidunt sequi consectetur.
+            Veritatis, beatae possimus aspernatur provident inventore, porro
+            dolorum alias eos, officiis quos animi repudiandae quo. Eos unde
+            totam cum ea tempore sequi praesentium delectus quod asperiores,
+            aspernatur accusamus quos explicabo vitae repudiandae reprehenderit
+            impedit magni odio rerum quibusdam similique cumque maxime quo?
+            Aspernatur tenetur, hic sint, accusamus ullam, iste illum in
+            possimus fugiat alias exercitationem ducimus reprehenderit suscipit?
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import spartan from "@/assets/scroll/pin/spartan.svg";
+import castle from "@/assets/scroll/pin/castle.svg";
+import daVinci from "@/assets/scroll/pin/da-vinci.svg";
+import airplane from "@/assets/scroll/pin/airplane.svg";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default {
+  name: "Pin",
+
+  data() {
+    return {
+      src: [spartan, castle, daVinci, airplane],
+
+      dates: ["-3300 | 476", "476 | 1492", "1492 | 1792", "1792 | AJD"],
+    };
+  },
+
+  mounted() {
+    ScrollTrigger.create({
+      trigger: ".pin-sections",
+      pin: ".pin-tooltip",
+      start: "top 40%",
+      end: "bottom 55%",
+    });
+
+    const sections = document.querySelectorAll(".pin-section");
+
+    sections.forEach((section, index) => {
+      console.log(index);
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top center",
+        end: "bottom center",
+        onEnter: () => {
+          gsap.set(".pin-tooltip-img", {
+            attr: { src: this.src[index] },
+          });
+          gsap.set(".pin-tooltip p", {
+            innerText: this.dates[index],
+          });
+        },
+        onEnterBack: () => {
+          gsap.set(".pin-tooltip-img", {
+            attr: { src: this.src[index] },
+          });
+          gsap.set(".pin-tooltip p", {
+            innerText: this.dates[index],
+          });
+        },
+      });
+    });
+  },
+};
+</script>
+
+<style scoped>
+.pin-project {
+  font-family: Montserrat, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+.pin-home {
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(to right, #eacda3, #d6ae7b);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #222;
+}
+
+.pin-home h1 {
+  font-family: Playfair Display, sans-serif;
+  font-size: clamp(35px, 5vw, 80px);
+  text-align: center;
+  text-transform: uppercase;
+  padding-bottom: 100px;
+}
+
+.pin-content {
+  padding: 20px 10px;
+  max-width: 700px;
+  margin: 0 auto;
+}
+
+.pin-content h2 {
+  font-family: Playfair Display, sans-serif;
+  font-size: clamp(45px, 3vw, 70px);
+  padding: 30px 0 20px;
+}
+.pin-content p {
+  font-size: 26px;
+}
+
+.pin-s1 {
+  background: rgb(243, 223, 192);
+}
+.pin-s2 {
+  background: rgb(224, 241, 251);
+}
+.pin-s3 {
+  background: rgb(223, 253, 223);
+}
+.pin-s4 {
+  background: rgb(245, 213, 132);
+}
+
+.pin-tooltip {
+  background: #fff;
+  width: 155px;
+  height: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: auto;
+  position: absolute;
+  padding: 10px;
+  border-radius: 5px;
+  text-transform: uppercase;
+}
+.pin-tooltip img {
+  width: 50%;
+  object-fit: cover;
+  object-position: center;
+  margin-bottom: 5px;
+}
+
+@media screen and (max-width: 1030px) {
+  .pin-content {
+    padding: 20px 10px;
+    max-width: 100%;
+    margin: 0 110px;
+  }
+  .pin-content p {
+    font-size: 20px;
+  }
+  .pin-tooltip {
+    background: #fff;
+    width: 100px;
+    height: 100px;
+  }
+  .tooltip img {
+    width: 40px;
+    height: 40px;
+  }
+  .pin-tooltip p {
+    font-size: 14px;
+  }
+}
+</style>
