c6b6a88e67c50cefb4557ecfa2d79bf99e1cbc35
AlenaAlyona <izakson91@gmail.com>
2021-10-29 02:39:25 +0200

    task: create view for css scrollable image project

---
+++ src/views/scroll/Image.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="image-box b1">
+    <img src="@/assets/scroll/image/img1.jpg" alt="mug on a book" />
+  </div>
+
+  <div class="image-box b2">
+    <img src="@/assets/scroll/image/img2.jpg" alt="red forest" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Image",
+
+  mounted() {},
+};
+</script>
+
+<style scoped>
+.image-box {
+  position: relative;
+  height: 100vh;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
+}
+
+img {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: fixed;
+  object-fit: cover;
+  height: 600px;
+  width: 400px;
+}
+
+.b1 {
+  background-color: #e27d60;
+}
+
+.b2 {
+  background-color: #85cdca;
+}
+</style>
