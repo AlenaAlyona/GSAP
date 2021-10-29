8e784d6b263200d088374ed7762cfa2e51d4e3ee
AlenaAlyona <izakson91@gmail.com>
2021-10-29 05:20:15 +0200

    task: add content to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <a
+      href="https://github.com/AlenaAlyona"
+      target="_blank"
+      class="home-link github"
+      >github</a
+    >
+    <img
+      src="@/assets/home/github.jpeg"
+      class="home-img"
+      alt="github profile picture"
+    />
+    <a
+      href="https://www.linkedin.com/in/alena-izakson/"
+      target="_blank"
+      class="home-link"
+      >linkedin</a
+    >
+  </div>
 </template>
 
 <script>
@@ -12,6 +30,35 @@ export default {
 .home {
   background: #161616;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.home-link {
+  text-decoration: none;
+  text-transform: uppercase;
+  color: #fff;
+  font-size: 50px;
+  padding: 10px;
+  z-index: 2;
+}
+
+.home-img {
+  z-index: 1;
+  height: 150px;
+  width: 150px;
+  display: none;
+  position: fixed;
+  opacity: 0.5;
+  padding-bottom: 150px;
+  padding-left: 150px;
+}
+
+.github:hover + .home-img {
+  display: block;
 }
 </style>
 >
